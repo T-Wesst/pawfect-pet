e455446a393236f83edd3190ecdfc3b3935a8a6f
T-Wesst <code.daddy17@gmail.com>
2020-07-23 00:24:56 -0700

    query api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import myData from './data.json';
+import React, { useState, useEffect } from 'react';
 import Cards from './components/Cards';
-// import axios from 'axios';
-// import React, { useState, useEffect } from 'react';
-// const token = process.env.REACT_APP_TOKEN;
+import axios from 'axios';
+const token = process.env.REACT_APP_TOKEN;
 
 export default function App() {
-  // const [data, setData] = useState({ hits: [] });
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios.get('/animals?type=dog', {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  //     setData(result.data);
-  //   };
-  //   fetchData();
-  // });
+  const [data, setData] = useState({ animals: [] });
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('animals?type=dog', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setData(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
-      <Cards {...myData} />
+      <Cards {...data} />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,29 +2,36 @@ import React from 'react';
 
 export default function Card(props) {
   const { name, status, published_at, size, age, gender, coat, tags } = props;
-  const { breeds, attributes, environment, contact, description } = props;
+  const {
+    breeds: { primary, secondary, tertiary, mixed },
+    attributes: { house_trained, shots_current, spayed_neutered },
+    environment: { children, dogs, cats },
+    contact: { address: { city, state }, phone, email},
+    photos,
+    description,
+  } = props;
   return (
     <div>
       <h1>{name}</h1>
-      <img src={props.photos[0]['small']} alt={`${name}`} />
+      <img src={photos[0]['small']} alt={`${name}`} />
       <ul>
         <li>Status: {status}</li>
         <li>Date Posted: {published_at}</li>
         <li>Size: {size}</li>
-        <li>Breed: {breeds.primary}</li>
-        <li>Spayed/Neutered: {attributes.spayed_neutered}</li>
-        <li>House Trained: {attributes.house_trained}</li>
-        <li>Shot Currents: {attributes.shots_current}</li>
+        <li>Breed: {(primary, secondary, tertiary, mixed)}</li>
+        <li>Spayed/Neutered: {spayed_neutered}</li>
+        <li>House Trained: {house_trained}</li>
+        <li>Shot Currents: {shots_current}</li>
         <li>Age: {age}</li>
         <li>Gender: {gender}</li>
         <li>Coat: {coat}</li>
-        <li>Environment children: {environment.children}</li>
-        <li>Environment dogs: {environment.dogs}</li>
-        <li>Environment cats: {environment.cats}</li>
-        <li>Phone: {contact.phone}</li>
-        <li>Email: {contact.email}</li>
-        <li>City: {contact.address.city}</li>
-        <li>State: {contact.address.state}</li>
+        <li>Environment children: {children}</li>
+        <li>Environment dogs: {dogs}</li>
+        <li>Environment cats: {cats}</li>
+        <li>Phone: {phone}</li>
+        <li>Email: {email}</li>
+        <li>City: {city}</li>
+        <li>State: {state}</li>
       </ul>
       <ul>
         <li>tags: {tags}</li>
