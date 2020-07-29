d0883a6e3d47605bbd9da07869e1e14df53c88b1
T-Wesst <code.daddy17@gmail.com>
2020-07-28 23:08:18 -0700

    add placeholder image and conditionally render fields

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,41 +1,46 @@
 import React from 'react';
+import placeholder from '../images/placholder.png'
 
 export default function Card(props) {
   const { name, status, published_at, size, age, gender, coat, tags } = props;
   const {
-    breeds: { primary, secondary, tertiary, mixed },
+    breeds: { primary },
     attributes: { house_trained, shots_current, spayed_neutered },
     environment: { children, dogs, cats },
-    contact: { address: { city, state }, phone, email},
+    contact: { address, phone, email},
     photos,
     description,
   } = props;
   return (
     <div>
       <h1>{name}</h1>
-      <img src={photos[0]['small']} alt={`${name}`} />
+       {photos.length > 0 && photos[0]['small'] ?
+      <img src={photos[0]["small"]} alt={name} /> : <img src={placeholder} alt="placeholder" />
+    }
       <ul>
         <li>Status: {status}</li>
         <li>Date Posted: {published_at}</li>
         <li>Size: {size}</li>
-        <li>Breed: {(primary, secondary, tertiary, mixed)}</li>
+        <li>Breed: {primary}</li>
         <li>Spayed/Neutered: {spayed_neutered}</li>
-        <li>House Trained: {house_trained}</li>
-        <li>Shot Currents: {shots_current}</li>
+        <li>House Trained: {house_trained ? 'yes': 'no'}</li>
+        <li>Shot Currents: {shots_current ? 'yes': 'no'}</li>
         <li>Age: {age}</li>
         <li>Gender: {gender}</li>
-        <li>Coat: {coat}</li>
-        <li>Environment children: {children}</li>
-        <li>Environment dogs: {dogs}</li>
-        <li>Environment cats: {cats}</li>
+        {coat && <li>Coat: {coat}</li>}
+        <li>Child Friendly: {children ? 'yes': 'no'}</li>
+        <li>Dog Friendly: {dogs ? 'yes': 'no'}</li>
+        <li>Cat Friendly: {cats ? 'yes': 'no'}</li>
         <li>Phone: {phone}</li>
         <li>Email: {email}</li>
-        <li>City: {city}</li>
-        <li>State: {state}</li>
+        <li>City: {address.city}</li>
+        <li>State: {address.state}</li>
       </ul>
+        {tags.length > 0 && (
       <ul>
         <li>tags: {tags}</li>
       </ul>
+        )}
       <p>{description}</p>
     </div>
   );
